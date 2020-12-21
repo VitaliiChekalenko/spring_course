767f3c97bf1fdf2ff0ab7f10ce6cc3eb86b0bb12
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-12-21 18:20:36 +0200

    Dependency Injection (DI) DI Values, HardCode || ver. 1.0

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -3,8 +3,27 @@ package spring_introduction;
 public class Person {
 
     private Pet pet;
+    private String surname;
+    private int age;
 
-//    public Person(Pet pet){
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+            return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
+    }
+    //    public Person(Pet pet){
 //        System.out.println("Person bean is created");
 //        this.pet = pet;
 //    }
--- src/spring_introduction/Test3.java
+++ src/spring_introduction/Test3.java
@@ -14,6 +14,9 @@ public class Test3 {
 
         person.callYourPet();
 
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+
         context.close();
     }
 }
