35897f375d127aaac0a767f55fd87dbc4391f360
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-12-24 17:19:49 +0200

    Bean scope. Singleton, prototype  || ver. 1.0

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -2,6 +2,16 @@ package spring_introduction;
 
 public class Dog implements Pet{
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Dog() {
         System.out.println("Dog bean is created");
     }
