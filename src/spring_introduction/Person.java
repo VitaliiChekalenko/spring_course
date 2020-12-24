257aae4a148ce7cae4de07bef7793b43da50bb4f
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-12-24 23:16:01 +0200

    @Autowired annotation for setter or any method  || ver. 1.0

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -29,15 +29,24 @@ public class Person {
         this.age = age;
     }
 
+    public Pet getPet() {
+        return pet;
+    }
     @Autowired
-        public Person(Pet pet){
-        System.out.println("Person bean is created");
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
 
-//    public Person(){
+    //    @Autowired
+//        public Person(Pet pet){
 //        System.out.println("Person bean is created");
-//            }
+//        this.pet = pet;
+//    }
+
+    public Person(){
+        System.out.println("Person bean is created");
+            }
 //    // pet -> setPet
 //    public void setPet(Pet pet) {
 //        System.out.println("Class Person: set Pet");
