3b009a40c20c301543d1d614952a12a6fcb82326
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-12-24 23:07:57 +0200

    @Autowired annotation for constructor  || ver. 1.0

--- src/spring_introduction/ConfigWithAnnotations1.java
+++ src/spring_introduction/ConfigWithAnnotations1.java
@@ -6,8 +6,12 @@ public class ConfigWithAnnotations1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
 
-        Cat myCat = context.getBean("catBean", Cat.class);
-        myCat.say();
+
+
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+//        Cat myCat = context.getBean("catBean", Cat.class);
+//        myCat.say();
 
         context.close();
     }
--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,5 +1,10 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component("personBean")
 public class Person {
 
     private Pet pet;
@@ -23,19 +28,21 @@ public class Person {
         System.out.println("Class Person: set age");
         this.age = age;
     }
-    //    public Person(Pet pet){
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
 
-    public Person(){
+    @Autowired
+        public Person(Pet pet){
         System.out.println("Person bean is created");
-            }
-    // pet -> setPet
-    public void setPet(Pet pet) {
-        System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
+
+//    public Person(){
+//        System.out.println("Person bean is created");
+//            }
+//    // pet -> setPet
+//    public void setPet(Pet pet) {
+//        System.out.println("Class Person: set Pet");
+//        this.pet = pet;
+//    }
 
     public void  callYourPet(){
         System.out.println("Hello, my lovely pet!");
