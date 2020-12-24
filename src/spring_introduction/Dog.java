f0a04cfcdee46558003e1c5a102c1fd08585fd81
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-12-24 18:21:10 +0200

    Init and destroy methods  || ver. 1.0

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -2,15 +2,15 @@ package spring_introduction;
 
 public class Dog implements Pet{
 
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+//    private String name;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public Dog() {
         System.out.println("Dog bean is created");
@@ -19,5 +19,14 @@ public class Dog implements Pet{
     @Override
     public void say(){
         System.out.println("Bow-Wow");
+    }
+
+    protected void init(){
+
+        System.out.println("Class Dog: ini method");
+    }
+
+    private void destroy(){
+        System.out.println("Class Dog: destroy method");
     }
 }
--- src/spring_introduction/Test4.java
+++ src/spring_introduction/Test4.java
@@ -6,13 +6,13 @@ public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-        Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Belka");
-        Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Strelka");
-
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
+//        Dog myDog = context.getBean("myPet", Dog.class);
+//        myDog.setName("Belka");
+//        Dog yourDog = context.getBean("myPet", Dog.class);
+//        yourDog.setName("Strelka");
+//
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 
 //        System.out.println("Переменные ссылаются на один и тот же объект? " + (myDog==yourDog));
 //        System.out.println(myDog);
