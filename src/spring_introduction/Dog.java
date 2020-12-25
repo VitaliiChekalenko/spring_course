285c343019d23c0fcfca6fd17bb042f5b837f3a8
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-12-25 17:42:23 +0200

    Configuration Spring Container via Java code. Config class. @Configuration, ComponentScan || ver. 1.0

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -30,13 +30,13 @@ public class Dog implements Pet {
         System.out.println("Bow-Wow");
     }
 
-    @PostConstruct
-    protected void init() {
-        System.out.println("Class Dog: init method");
-    }
-
-    @PreDestroy
-    private void destroy() {
-        System.out.println("Class Dog: destroy method");
-    }
+//    @PostConstruct
+//    protected void init() {
+//        System.out.println("Class Dog: init method");
+//    }
+//
+//    @PreDestroy
+//    private void destroy() {
+//        System.out.println("Class Dog: destroy method");
+//    }
 }
---
+++ src/spring_introduction/Test6.java
@@ -0,0 +1,16 @@
+package spring_introduction;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test6 {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Person person = context.getBean("personBean", Person.class);
+
+        person.callYourPet();
+
+    }
+
+}
